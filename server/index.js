64bd3d6a9d56c23dd65d8b639a113012a0e0049d
Alexander Runemark <alexanderrunemark@Alexanders-MBP.montania.lan>
2019-03-26 11:19:01 +0100

    url api, start

--- server/index.js
+++ server/index.js
@@ -10,9 +10,11 @@ app.use(bodyParser.json());
 
 const products = require('./routes/api/productApi');
 const media = require('./routes/api/media');
+const search = require('./routes/api/url');
 
 app.use('/api/products', products);
 app.use('/wp/v2/media', media);
+app.use('/wp/v2/search', search);
 
 
 
---
+++ server/routes/api/url.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get posts
+router.get('/:id', async (req,res) =>{
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({name: req.params.id}).toArray());
+});
+
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
+    return client.db('gutenberg').collection('url');
+}
+
+module.exports = router;
