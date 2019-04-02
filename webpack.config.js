d0e696c691633d01b0549af8cba41815d5abb869
Alexander Runemark <alexanderrunemark@Alexanders-MBP.montania.lan>
2019-04-02 10:35:17 +0200

    testing testing

--- webpack.config.js
+++ webpack.config.js
@@ -192,6 +192,7 @@ module.exports = {
                     },
                 ],
             },
+            
             /* {
               test: /editor\.s?css$/,
               include: [
@@ -233,6 +234,135 @@ module.exports = {
                     ],
                 }),
             },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000,
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+              },
+              // Process application JS with Babel.
+              // The preset includes JSX, Flow, and some ESnext features.
+              {
+                test: /\.(js|mjs|jsx)$/,
+                include: paths.appSrc,
+                loader: require.resolve('babel-loader'),
+                options: {
+                  customize: require.resolve(
+                    'babel-preset-react-app/webpack-overrides'
+                  ),
+    
+                  plugins: [
+                    [
+                      require.resolve('babel-plugin-named-asset-import'),
+                      {
+                        loaderMap: {
+                          svg: {
+                            ReactComponent: '@svgr/webpack?-prettier,-svgo![path]',
+                          },
+                        },
+                      },
+                    ],
+                  ],
+                  // This is a feature of `babel-loader` for webpack (not Babel itself).
+                  // It enables caching results in ./node_modules/.cache/babel-loader/
+                  // directory for faster rebuilds.
+                  cacheDirectory: true,
+                  // Don't waste time on Gzipping the cache
+                  cacheCompression: false,
+                },
+              },
+              // Process any JS outside of the app with Babel.
+              // Unlike the application JS, we only compile the standard ES features.
+              {
+                test: /\.(js|mjs)$/,
+                exclude: /@babel(?:\/|\\{1,2})runtime/,
+                loader: require.resolve('babel-loader'),
+                options: {
+                  babelrc: false,
+                  configFile: false,
+                  compact: false,
+                  presets: [
+                    [
+                      require.resolve('babel-preset-react-app/dependencies'),
+                      { helpers: true },
+                    ],
+                  ],
+                  cacheDirectory: true,
+                  // Don't waste time on Gzipping the cache
+                  cacheCompression: false,
+    
+                  // If an error happens in a package, it's possible to be
+                  // because it was compiled. Thus, we don't want the browser
+                  // debugger to show the original code. Instead, the code
+                  // being evaluated would be much more helpful.
+                  sourceMaps: false,
+                },
+              },
+              // "postcss" loader applies autoprefixer to our CSS.
+              // "css" loader resolves paths in CSS and adds assets as dependencies.
+              // "style" loader turns CSS into JS modules that inject <style> tags.
+              // In production, we use a plugin to extract that CSS to a file, but
+              // in development "style" loader enables hot editing of CSS.
+              // By default we support CSS Modules with the extension .module.css
+              {
+                test: cssRegex,
+                exclude: cssModuleRegex,
+                use: getStyleLoaders({
+                  importLoaders: 1,
+                }),
+              },
+              // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+              // using the extension .module.css
+              {
+                test: cssModuleRegex,
+                use: getStyleLoaders({
+                  importLoaders: 1,
+                  modules: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                }),
+              },
+              // Opt-in support for SASS (using .scss or .sass extensions).
+              // Chains the sass-loader with the css-loader and the style-loader
+              // to immediately apply all styles to the DOM.
+              // By default we support SASS Modules with the
+              // extensions .module.scss or .module.sass
+              {
+                test: sassRegex,
+                exclude: sassModuleRegex,
+                use: getStyleLoaders({ importLoaders: 2 }, 'sass-loader'),
+              },
+              // Adds support for CSS Modules, but using SASS
+              // using the extension .module.scss or .module.sass
+              {
+                test: sassModuleRegex,
+                use: getStyleLoaders(
+                  {
+                    importLoaders: 2,
+                    modules: true,
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
+                  'sass-loader'
+                ),
+              },
+              // "file" loader makes sure those assets get served by WebpackDevServer.
+              // When you `import` an asset, you get its (virtual) filename.
+              // In production, they would get copied to the `build` folder.
+              // This loader doesn't use a "test" so it will catch all modules
+              // that fall through the other loaders.
+              {
+                // Exclude `js` files to keep "css" loader working as it injects
+                // its runtime that would otherwise be processed through "file" loader.
+                // Also exclude `html` and `json` extensions so they get processed
+                // by webpacks internal loaders.
+                exclude: [/\.(js|mjs|jsx)$/, /\.html$/, /\.json$/],
+                loader: require.resolve('file-loader'),
+                options: {
+                  name: 'static/media/[name].[hash:8].[ext]',
+                },
+              },
+            
         ],
     },
     plugins: [
