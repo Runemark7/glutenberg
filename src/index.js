733b276af2ec7ac6cd51a8dcfb6922389b48e316
Alexander Runemark <alexanderrunemark@Alexanders-MBP.montania.lan>
2019-04-03 13:34:15 +0200

    api-fetch undefined

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import './globals';
+import Editor from './pages/editor';
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/posts" component={ Editor } />
+      <Route exact path="/pages" component={ Editor } />
+      <Route component={ Editor } />
+    </Switch>
+  </BrowserRouter>
+),document.getElementById('root'));
 
 /**
  * Internal dependencies
